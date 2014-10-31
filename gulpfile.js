952c9d37aefdbd770b1520cf6550435bf329724e
erbear <erbert.b@gmail.com>
2014-11-01 00:07:16 +0100

    server with livereload added

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp')
     sass = require('gulp-ruby-sass')
+    connect = require('gulp-connect');
 
 gulp.task('stylesheets', function() {
   return gulp.src('dist/stylesheets/**/*.sass')
     .pipe(sass({ style: 'expanded' }))
     .pipe(gulp.dest('public/stylesheets'))
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(connect.reload())
+});
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true
+  });
 });
 
 gulp.task('watch', function() {
@@ -14,3 +21,5 @@ gulp.task('watch', function() {
   gulp.watch('dist/stylesheets/**/*.sass', ['stylesheets']);
 
 });
+
+gulp.task('default', ['stylesheets', 'webserver', 'watch'])
