83b24a2d099701106e3bc9a523adb2f2a36b4375
erbear <erbert.b@gmail.com>
2014-11-01 00:14:32 +0100

    slim customized

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp')
     sass = require('gulp-ruby-sass')
-    connect = require('gulp-connect');
+    connect = require('gulp-connect')
+    slim = require("gulp-slim");
 
 gulp.task('stylesheets', function() {
   return gulp.src('dist/stylesheets/**/*.sass')
     .pipe(sass({ style: 'expanded' }))
     .pipe(gulp.dest('public/stylesheets'))
-    .pipe(connect.reload())
+    .pipe(connect.reload());
+});
+
+gulp.task('templates', function(){
+  gulp.src("dist/templates/**/*.slim")
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest("public/templates"))
+    .pipe(connect.reload());
 });
 
 gulp.task('webserver', function() {
@@ -19,7 +29,8 @@ gulp.task('watch', function() {
 
   // Watch .sass files
   gulp.watch('dist/stylesheets/**/*.sass', ['stylesheets']);
+  gulp.watch('dist/templates/**/*.slim', ['templates']);
 
 });
 
-gulp.task('default', ['stylesheets', 'webserver', 'watch'])
+gulp.task('default', ['stylesheets', 'templates', 'webserver', 'watch'])
